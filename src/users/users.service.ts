171328481c62c0a1aa77697daa290c9921e76bfa
Mattias Duarte <duartemattias3@gmail.com>
2024-09-13 17:32:57 -0400

    simple CRUD for register users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor( private prisma: PrismaService ){}
+
+  async create(createUserDto: CreateUserDto) {
+
+    const user = await this.prisma.user.create({
+      data: createUserDto
+    })
+
+    return user;  
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    const users = await this.prisma.user.findMany();
+  
+    return users; 
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    
+    const user = await this.prisma.user.findFirst({
+      where: {
+        id
+      }
+    }) 
+    
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    
+    const user = this.prisma.user.update({
+      where: { id },
+      data: updateUserDto
+    })
+
+    return user;
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    
+    await this.prisma.user.delete({ where: { id }})
+
+    return "Usuario eliminado";
+
   }
 }
